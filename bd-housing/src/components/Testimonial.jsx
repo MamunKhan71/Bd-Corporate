210041f56ff96dff57cd9b34805dd7d4df3c9679
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-04-10 04:42:18 +0600

    Added dynamic Testimonials using Axios

--- bd-housing/src/components/Testimonial.jsx
+++ bd-housing/src/components/Testimonial.jsx
@@ -4,8 +4,11 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 import { Pagination } from 'swiper/modules';
+import { useContext } from 'react';
+import { ContentContext } from '../provider/ContextProvider';
 
 export default function Testimonial() {
+    const { testimonial } = useContext(ContentContext)
     return (
         <>
             <div>
@@ -18,22 +21,20 @@ export default function Testimonial() {
                     modules={[Pagination]}
                     className="mySwiper my-24"
                 >
-                    <SwiperSlide>
-                        <>
-                            <div className='text-white bg-primary h-[50dvh] flex items-center justify-center flex-col space-y-4'>
-                                <h1 className='text-center font-bold text-2xl'>Testimonials</h1>
-                                <FaQuoteLeft className='text-3xl text-gray-400' />
-                                <p className='w-[780px] text-center text-xl'>What a great experience! I have visited one of the workshops and
-                                    attended a masterclass, and both were super useful for young
-                                    designers. Highly recommended.</p>
-                                <FaQuoteRight className='text-3xl text-gray-400' />
-                                <p className='text-base'>Ali Tufan</p>
-                                <p className='text-base'>Product Manager</p>
-                            </div>
-                        </>
-                    </SwiperSlide>
-                    <SwiperSlide>Slide 2</SwiperSlide>
-                    <SwiperSlide>Slide 3</SwiperSlide>
+                    {
+                        testimonial.map(test => <>
+                            <SwiperSlide>
+                                <div className='text-white bg-primary h-[50dvh] flex items-center justify-center flex-col space-y-4'>
+                                    <h1 className='text-center font-bold text-2xl'>Testimonial</h1>
+                                    <FaQuoteLeft className='text-3xl text-gray-400' />
+                                    <p className='w-[780px] text-center text-xl'>{test.description}</p>
+                                    <FaQuoteRight className='text-3xl text-gray-400' />
+                                    <p className='text-base'>{test.author}</p>
+                                    <p className='text-base'>{test.position}</p>
+                                </div>
+                            </SwiperSlide>
+                        </>)
+                    }
                 </Swiper>
             </div>
         </>
--- bd-housing/src/provider/ContextProvider.jsx
+++ bd-housing/src/provider/ContextProvider.jsx
@@ -1,12 +1,15 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
-
 export const ContentContext = createContext()
 const ContextProvider = ({children}) => {
     const [testimonial, setTestimonial] = useState([])
     useEffect(()=>{
-        fetch()
+        axios.get('testimonial.json')
+            .then(res => setTestimonial(res.data.testimonials))
+            .catch(error => console.log(error))
     },[])
-    const contents = {}
+    console.log(testimonial)
+    const contents = {testimonial}
     return (
         <div>
             <ContentContext.Provider value={contents}>
