a4dac39b5679775565f9085b4e58e2900cab769e
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-04-15 15:05:02 +0600

    Redesigned Error Page

--- bd-housing/src/pages/NotFound.jsx
+++ bd-housing/src/pages/NotFound.jsx
@@ -4,26 +4,35 @@ import 'animate.css';
 const NotFound = () => {
     return (
         <>
-            <div className="">
-                <div className="min-h-screen flex items-center justify-center">
-                    <div className="bg-base-200 text-primary shadow overflow-hidden sm:rounded-lg pb-8">
-                        <div className="border-gray-200 text-center p-24 animate__animated animate__zoomIn">
-                            <h1 className="text-9xl font-bold text-primary">404</h1>
-                            <h1 className="text-6xl font-medium py-8 text-gray-500">oops! Page not found</h1>
-                            <p className="text-2xl pb-8 px-12 font-medium text-gray-500">
-                                Oops! The page you are looking for does not exist. It might have
-                                been moved or deleted.
-                            </p>
-                            <Link to="/" className="bg-gradient-to-r bg-primary hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-6 py-3 rounded-md mr-6">
-                                Home
-                            </Link>
-                            <Link to="/login" className="bg-gradient-to-r  bg-primary hover:from-red-500 hover:to-red-500 text-white font-semibold px-6 py-3 rounded-md">
-                                Login
-                            </Link>
+            <section className="relative z-10 bg-primary h-dvh flex items-center justify-center bg-[url(/images/Hexagon.svg)]">
+                <div className="container mx-auto">
+                    <div className="-mx-4 flex">
+                        <div className="w-full px-4 animate__animated animate__bounceIn">
+                            <div className="mx-auto max-w-[400px] text-center">
+                                <h2
+                                    className="mb-2 text-9xl font-bold leading-none text-white"
+                                >
+                                    404
+                                </h2>
+                                <h4
+                                    className="mb-3 text-2xl font-semibold leading-tight text-white"
+                                >
+                                    Oops! That page can’t be found
+                                </h4>
+                                <p className="mb-8 text-lg text-white">
+                                    The page you are looking for it maybe deleted
+                                </p>
+                                <Link to="/"
+                                    className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white transition hover:bg-white hover:text-primary"
+                                >
+                                    Go To Home
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                
+            </section>
         </>
 
     );
