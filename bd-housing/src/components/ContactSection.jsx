ce33c74cea4e066952556e2505c07764204f1076
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2024-04-16 08:56:22 +0600

    Responsiveness Improvements

--- bd-housing/src/components/ContactSection.jsx
+++ bd-housing/src/components/ContactSection.jsx
@@ -2,14 +2,14 @@ import { IoIosCall } from "react-icons/io";
 import { TbArrowUpRight } from "react-icons/tb";
 const ContactSection = () => {
     return (
-        <div className="container mx-auto flex flex-col space-y-4 lg:flex-row items-center py-12 lg:py-0 justify-around bg-[#F7F7F7] px-6 lg:p-12 rounded-none">
+        <div className="container mx-auto flex flex-col lg:flex-row space-y-4 lg:space-y-0 items-center justify-around bg-[#F7F7F7] p-12 mb-24">
             <div className="text-center lg:text-left">
-                <h1 className="text-2xl lg:text-3xl font-bold text-primary">Need help? Talk to our expert.</h1>
+                <h1 className="text-3xl font-bold text-primary">Need help? Talk to our expert.</h1>
                 <p className="text-primary">Talk to our experts or Browse through more properties.</p>
             </div>
             <div className="flex flex-col lg:flex-row gap-4 justify-end items-center">
-                <button className="bg-primary btn text-white inline-flex gap-2 items-center text-lg rounded-none w-full">Contact Us <TbArrowUpRight/></button>
-                <button className="border-primary btn border-2 text-primary bg-transparent rounded-none inline-flex gap-2 items-center text-lg w-full"><IoIosCall/>920 851 8585</button>
+                <button className="bg-primary btn text-white inline-flex gap-2 items-center text-lg rounded-none w-full lg:w-auto">Contact Us <TbArrowUpRight /></button>
+                <button className="border-primary btn border-2 text-primary bg-transparent inline-flex gap-2 rounded-none items-center text-lg w-full lg:w-auto"><IoIosCall />920 851 8585</button>
             </div>
         </div>
     );
--- bd-housing/src/components/Footer.jsx
+++ bd-housing/src/components/Footer.jsx
@@ -1,31 +1,34 @@
 const Footer = () => {
     return (
-        <div className="bg-primary mt-48">
-            <footer className="footer p-10 container mx-auto text-base-content">
-                <aside className="text-white">
+        <div className="bg-primary mt-12 lg:mt-48">
+            <footer className="footer p-4 lg:p-10 container mx-auto text-base-content">
+                <aside className="text-white flex flex-col gap-2">
                     <img className="w-12 h-12" src="/logo.png" alt="" />
                     <p>BDCorporate<br />Providing reliable real estate since 1999</p>
                 </aside>
-                <nav className="text-white">
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
-                </nav>
-                <nav className="text-white">
-                    <h6 className="footer-title">Company</h6>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
-                </nav>
-                <nav className="text-white">
-                    <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
-                </nav>
+                <div className="flex w-full justify-between flex-wrap">
+                    <nav className="text-white flex flex-col gap-2">
+                        <h6 className="footer-title">Services</h6>
+                        <a className="link link-hover">Branding</a>
+                        <a className="link link-hover">Design</a>
+                        <a className="link link-hover">Marketing</a>
+                        <a className="link link-hover">Advertisement</a>
+                    </nav>
+
+                    <nav className="text-white flex flex-col gap-2">
+                        <h6 className="footer-title">Company</h6>
+                        <a className="link link-hover">About us</a>
+                        <a className="link link-hover">Contact</a>
+                        <a className="link link-hover">Jobs</a>
+                        <a className="link link-hover">Press kit</a>
+                    </nav>
+                    <nav className="text-white flex flex-col gap-2">
+                        <h6 className="footer-title">Legal</h6>
+                        <a className="link link-hover">Terms of use</a>
+                        <a className="link link-hover">Privacy policy</a>
+                        <a className="link link-hover">Cookie policy</a>
+                    </nav>
+                </div>
             </footer>
             <div className="bg-primary">
                 <footer className="footer footer-center p-10 bg-primary text-primary-content">
